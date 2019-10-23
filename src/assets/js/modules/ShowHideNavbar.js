3bc482e88d8768699669bf27fcf085dc227858c8
Lawrence <locaulker@gmail.com>
2019-10-23 01:31:53 -0400

    completed back-to-top button

--- src/assets/js/modules/ShowHideNavbar.js
+++ src/assets/js/modules/ShowHideNavbar.js
@@ -10,11 +10,18 @@ class ShowHideNavbar {
     $( () => {
       $(window).scroll( () => {
         if( $(window).scrollTop() > 60 ) {
-        // (1). If scroll position is > 50px, display White Bg Nav
+        // (1a). If scroll position is > 50px, display White Bg Nav
         $("header").addClass("white-nav-top");
+
+        // (2a) Show back-to-top button
+        $("#back-to-top").fadeIn();
+
         } else {
-          // (2). Hide White Bg Nav
+          // (1b). Hide White Bg Nav
           $("header").removeClass("white-nav-top");
+
+          // (2b). Hide back-to-top button
+          $("#back-to-top").fadeOut();
         }
       });
 
